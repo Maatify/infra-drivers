633ca53595fdc29929080ff196f7e5cac8d526cd
Mohamed Abdulalim <megyptm@gmail.com>
2025-12-18 19:46:35 +0200

    feat(phaseD): enforce deterministic error boundaries in driver builders
    - Added InvalidDriverConfigException and MissingExtensionException
    - Linked builders to throw typed infrastructure exceptions
    - Wrapped native driver failures using DriverBuildException::fromThrowable

---
+++ src/Exception/InvalidDriverConfigException.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @copyright   ©2025 Maatify.dev
+ * @Library     maatify/infra-drivers
+ * @Project     maatify:infra-drivers
+ * @author      Mohamed Abdulalim (megyptm) <mohamed@maatify.dev>
+ * @since       2025-12-18 19:22
+ * @see         https://www.maatify.dev Maatify.dev
+ * @link        https://github.com/Maatify/infra-drivers view Project on GitHub
+ * @note        Distributed in the hope that it will be useful - WITHOUT WARRANTY.
+ */
+
+declare(strict_types=1);
+
+namespace Maatify\InfraDrivers\Exception;
+
+use InvalidArgumentException;
+
+final class InvalidDriverConfigException extends InvalidArgumentException
+{
+}
